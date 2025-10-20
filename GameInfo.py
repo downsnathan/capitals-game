b832e265c1d25f32585cc32867e3e77bf0addeda
Nathan <ndowns@umass.edu>
2025-10-20 14:14:14 -0400

    Enabled replaying, added comments, made capitals lowercase, added colab check to avoid errors running locally

--- GameInfo.py
+++ GameInfo.py
@@ -6,25 +6,30 @@ class GameInfo:
     def makeDicts(self, countries, capitals):
         file = open("Capitals.txt")
         for line in file:
-            
             split = line.split(",")
-            if split[1] == "Afghanistan":
+            if split[1] == "Afghanistan": #get rid of random characters added to start of file for some reason
                 split[0] = 0
             countries[int(split[0])] = split[1] 
             capitals[int(split[0])] = [split[-1][0:-1]] 
-            if len(split) > 3:
+            if len(split) > 3: #if more than one capital, add the rest to the list
                 for i in range(2, len(split) - 1):
                     capitals[int(split[0])].append(split[i])
 
+    def newGame(self):
+        for i in range(0, 198):
+            self.toAsk.append(i)
+        random.shuffle(self.toAsk)
+
     def __init__(self):
         self.score = 0
-        self.correctAnswers = 0
+        self.correct = 0
         self.maxCorrect = 197
         self.maxScore = 197 * 3
-        self.countryDict = {}
-        self.capitalDict = {}
+        self.countryDict = {} #map index to country
+        self.capitalDict = {} #map index to capital(s)
         self.toAsk = []
         self.incorrect = []
+        self.previousScores = {}
 
         self.makeDicts(self.countryDict, self.capitalDict)
         for i in range(0, 198):
--- Main.py
+++ Main.py
@@ -1,11 +1,20 @@
 import random
 from PIL import Image
 import matplotlib.pyplot as plt
-from IPython.core.display import clear_output
 from GameInfo import GameInfo
 import time
 
-def showMap(index):
+try:
+  import google.colab
+  IN_COLAB = True
+except:
+  IN_COLAB = False
+
+if IN_COLAB:
+    from IPython.core.display import clear_output
+
+
+def showMap(index): #index of country or "blank map" for map without any country highlighted
     def set_size(w,h, ax=None):
         """ w, h: width, height in inches """
         if not ax: ax=plt.gca()
@@ -28,50 +37,70 @@ def showMap(index):
     plt.show()
 
 def showScore(info):
-    print("Score: " + info.score + "/" + info.maxScore + "        Correct: " + info.correct + "/" + info.maxCorrect)
+    print("Correct: " + str(info.correct) + "/" + str(info.maxCorrect) + "        " + "Score: " + str(info.score) + "/" + str(info.maxScore))
 
-def questionAnswer(info, index):
-    correctAnswers = info.capitalDict[index]
+def askQuestion(info, index):
+    #info.capitalDict[index] = correct answers
     for posScore in range(3, 0, -1):
-        clear_output()
+        if IN_COLAB:
+            clear_output()
         showScore(info)
         showMap(index)
         if posScore < 3:
             hint = []
             for i in range(0, len(info.capitalDict[index])):
-                hint[i] = info.capitalDict[index][i][0] + "*" * len(info.capitalDict[index][i] - 1)
+                hint.append(info.capitalDict[index][i][0].upper() + ("*" * (len(info.capitalDict[index][i]) - 1)))
             if posScore == 1:
-                for i in range(0, len(info.capitalSict[index])):
-                    hint[i][-1] = info.capitalDict[index][i][-1]
-            print("Hint: " + hint)
+                for i in range(0, len(info.capitalDict[index])):
+                    hint[i] = hint[i][0:-1] + info.capitalDict[index][i][-1]
+            hintString = hint[0]
+            for i in range(1, len(hint)):
+                hintString += ", " + hint[i]
+
+            print("Hint: " + hintString)
         time.sleep(.5)
         response = input("Capital: ")
-        if response in info.capitalDict[index]:
+        if response.lower() in info.capitalDict[index]:
             print("Correct!")
             info.score += posScore
             info.correct += 1
             time.sleep(1)
-            info.capitalDict[index].remove(response)
-            if len(info.capitalDict[index]) == 0:
-                info.capitalDict.remove(index)
             return
         else:
             print("Incorrect")
             time.sleep(1)
-    info.capitalDict.remove(index)
+    print("You ran out of attempts")
 
 def game():
     info = GameInfo()
-    clear_output()
-    showMap("blank")
-    time.sleep(.5)
-    start = input("Type y to begin: ")
-    start = time.time()
-    while len(info.toAsk) > 0:
-        index = info.toAsk.pop(0)
-        questionAnswer(info, index)
-    clear_output()
-    showScore(info)
-    showMap("blank")
-    gameLength = int(time.time() - start)
-    print("Time: " + time)
+    if IN_COLAB:
+        clear_output()
+    startGame = True
+    while startGame:
+        showMap("blank")
+        time.sleep(.5)
+        start = input("Type y to begin: ")
+        start = time.time()
+        while len(info.toAsk) > 0:
+            index = info.toAsk.pop(0)
+            askQuestion(info, index)
+        if IN_COLAB:
+            clear_output()
+        showScore(info)
+        gameLength = int(time.time() - start)
+        info.previousScores[len(info.previousScores)] = (str(len(info.previousScores)), str(info.score), str(gameLength))
+        print("Time: " + str(gameLength))
+        showMap("blank")
+        time.sleep(4)
+        print("Attempt              Score               Time")
+        for i in info.previousScores:
+            print(i[0] + "                  " + i[1] + "                " + i[2])
+        print()
+        print()
+        response = input("Type y to try again or q to quit: ")
+        if response != "y":
+            startGame = False
+            print("Goodbye")
+            
+
+game()
